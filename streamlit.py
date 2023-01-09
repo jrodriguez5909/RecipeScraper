e74231885d2801c42058d14ffd5c1440de1869ef
jrodriguez5909 <jutiliano.rodriguez@gmail.com>
2023-01-09 15:02:41 +0100

    Updates to app frontend

--- streamlit.py
+++ streamlit.py
@@ -8,7 +8,7 @@ st.write("""# Recipe Scraper""")
 st.image("https://images.unsplash.com/photo-1577308856961-8e9ec50d0c67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMG9uJTIwdGFibGV8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60", caption="What's for dinner?")
 st.write("[See this app's GitHub ReadMe file for more info](%s)" % "https://github.com/jrodriguez5909/RecipeScraper#top-daily-stock-losers--trading-opportunities")
 st.write("""
-## App Instructions:
+## **App Instructions:**
 1. Populate the text box below with URLs for recipes you'd like to gather ingredient from - separate the URLs with commas e.g., https://www.hellofresh.nl/recipes/chicken-parmigiana-623c51bd7ed5c074f51bbb10, https://www.hellofresh.nl/recipes/quiche-met-broccoli-en-oude-kaas-628665b01dea7b8f5009b248
 2. Click **Grab ingredient list** to kick off the web scraping and creation of the ingredient shopping list dataset.
 3. Click **Download full csv file** link below if you'd like to download the ingredient shopping list dataset as a csv file.
