b0e19a6e986e482e062160f8d551511ef23652a8
jrodriguez5909 <jutiliano.rodriguez@gmail.com>
2023-01-12 09:45:41 +0100

    Added stqdm() to create_df() in main.py to render progress bar on streamlit frontend

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def create_df(recipes):
     """
     df_list = []
 
-    for recipe in recipes:
+    for recipe in stqdm(recipes):
         scraper = scrape_me(recipe)
         recipe_details = replace_measurement_symbols(scraper.ingredients())
 
